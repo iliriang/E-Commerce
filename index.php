c76267e83988e1a9c747ea85835945cdf61b073a
Ilirian <ig51913@ubt-uni.net>
2022-04-20 17:22:25 +0200

    changes

--- index.php
+++ index.php
@@ -284,7 +284,7 @@ include 'resources\dbResources.php';
               class="newsletter__input"
             />
 
-            <a href="#" class="button">Subscribe1</a>
+            <a href="#" class="button">Subscribe</a>
           </form>
         </div>
       </section>
